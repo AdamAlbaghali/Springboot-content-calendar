b9df4725223da6cd6ba56fafb7cc3302c6361f6a
AdamAlbaghali <adamalbaghali@gmail.com>
2024-02-15 13:53:37 -0500

    Added CommandLineRunner bean to execute data insertion logic and used Query derivation

---
+++ src/main/java/dev/adamalbaghali/contentcalendar/config/DataLoader.java
@@ -0,0 +1,14 @@
+package dev.adamalbaghali.contentcalendar.config;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+// @Component
+public class DataLoader implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Hello :wave:");
+    }
+    
+}
